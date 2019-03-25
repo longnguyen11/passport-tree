0a6e55272603bc444fc27b3086a64d6816144a66
Long Nguyen <caycham211@gmail.com>
2019-03-24 20:49:48 -0400

    scaffolding, seting up webpack etc..

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    console.log('APP');
+    return (
+      <div className="App">
+        RAA RAA RAA
+      </div>
+    );
+  }
+}
+
+export default App;
